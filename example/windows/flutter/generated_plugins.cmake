cd2d5c9e8b032944a649023e3f56533a94b98235
Ali Mustofa <mustofa@extrainteger.com>
2023-05-26 16:00:41 +0700

    fixing localizations

--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  flutter_localization
   share_plus
   url_launcher_windows
 )
--- lib/src/view/helper/chucker_ui_helper.dart
+++ lib/src/view/helper/chucker_ui_helper.dart
@@ -10,6 +10,7 @@ import 'package:chucker_flutter/src/view/helper/chucker_button.dart';
 import 'package:chucker_flutter/src/view/helper/colors.dart';
 import 'package:chucker_flutter/src/view/widgets/notification.dart'
     as notification;
+import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -151,7 +152,11 @@ class ChuckerUiHelper {
         builder: (context) => MaterialApp(
           key: const Key('chucker_material_app'),
           debugShowCheckedModeBanner: false,
-          localizationsDelegates: Localization.localizationsDelegates,
+          localizationsDelegates: const [
+            DefaultMaterialLocalizations.delegate,
+            DefaultWidgetsLocalizations.delegate,
+            DefaultCupertinoLocalizations.delegate,
+          ],
           supportedLocales: Localization.supportedLocales,
           locale: Localization.currentLocale,
           theme: ThemeData(
